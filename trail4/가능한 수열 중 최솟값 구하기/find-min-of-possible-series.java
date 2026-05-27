756522c9a029494761efd0670c80cab833237bbc
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-05-27 15:12:23 +0900

    [Compile Error] 가능한 수열 중 최솟값 구하기

--- trail4/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
+++ trail4/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
 
     static int n;
-    // static List<Integer> list = new ArrayList<>();
     static int[] arr;
     static boolean finish = false;
 
@@ -58,5 +57,33 @@ public class Main {
         dfs(idx+1);
         arr[idx] = 6;
         dfs(idx+1);
+    }
+
+    static boolean check(int idx) {
+        if (idx >= 6) {
+            boolean check = true;
+            for (int i=0; i<3; i++) {
+                if (arr[idx-6+i] != arr[idx-3+i]) {
+                    check = false;
+                }
+            }
+
+            if(check) return;
+        } 
+        
+        if (idx >= 4) {
+            boolean check = true;
+            for (int i=0; i<2; i++) {
+                if (arr[idx-4+i] != arr[idx-2+i]) {
+                    check = false;
+                }
+            }
+
+            if(check) return;
+        }
+
+        if (idx >= 2) {
+           if (arr[idx-2] == arr[idx-1]) return;
+        }
     }
 }
