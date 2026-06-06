a2ca12373e0520c1c61bd52e7aea98bbd68c44b3
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-06-06 21:40:32 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/', f.BOARD_ID, '/', FILE_ID, FILE_NAME, file_ext) as FILE_PATH
+from USED_GOODS_BOARD b join USED_GOODS_FILE f on b.BOARD_ID = f.BOARD_ID
+where views = (select max(views) from USED_GOODS_BOARD)
+order by FILE_ID DESC;
